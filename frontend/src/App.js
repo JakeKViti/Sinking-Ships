3b169ca1f1ae81e80837e59611eb291d6a76538e
JakeKViti <thevitij64@gmail.com>
2021-04-20 23:54:24 -0400

    add NavBar functionality to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import Board from './components/Board.js'
+import NavBar from './components/NavBar.js'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <Board/>
+        <NavBar />
+        <Route exact path="/board" component={Board} />
       </header>
     </div>
+    </Router>
   );
 }
 
