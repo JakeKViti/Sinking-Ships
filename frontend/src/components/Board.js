4c9b201b5a28a52d63e0b3116ebac7f10923d326
JakeKViti <thevitij64@gmail.com>
2021-04-20 23:47:24 -0400

    create Components folder and Board.js

---
+++ frontend/src/components/Board.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Board extends Component {
+    render() {
+        return (
+            "Future Board Will Render Here"
+        );
+    }
+}
+export default Board;
