a3d1ea2bebbb7d5d659e2e5bb5a90ee2c8da1f7a
JakeKViti <thevitij64@gmail.com>
2021-04-21 21:40:21 -0400

    create Game.js

---
+++ frontend/src/components/Game.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+    state = {
+        height: 10,
+        width: 10
+    }
+
+    render() {
+        const { height, width } = this.state;
+        return (
+          <div className="game">
+            <Board height={height} width={width} />
+          </div>
+        );
+      }
+    
+}
+
+export default Game;
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class NavBar extends React.Component {
     render() {
         return (
-            "This is Homepage"
+            "Press the play button to start the game or Rules to see how to play!"
         );
     }
 };
