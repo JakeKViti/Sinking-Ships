dd2861c12b766432282fee15a951acb5a4fe56dc
JakeKViti <thevitij64@gmail.com>
2021-04-23 14:02:08 -0400

    made placeShips and getRandomNumber functions

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -26,8 +26,27 @@ class Board extends Component {
         return data
     }
 
+    placeShips(data, height, width) {
+      let randomx, randomy = 0;
+      let smallShip = 0
+      while (smallShip < 2) {
+        randomx = this.getRandomNumber(width);
+        randomy = this.getRandomNumber(height);
+        debugger
+        }
+      }
+      
+      return (data);
+    }
+
+    getRandomNumber(number) {
+      let rando = Math.floor((Math.random() * 1000) + 1) % number;
+      return rando
+    }
+
     initBoardData(height, width) {
         let data = this.createEmptyArray(height, width);
+        data = this.placeShips(data, height, width);
         return data;
     }
 
