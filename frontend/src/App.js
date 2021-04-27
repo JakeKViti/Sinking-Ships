71dc7b9b525fe2c3081f3324316b6e0e1ae72715
JakeKViti <thevitij64@gmail.com>
2021-04-26 22:17:06 -0400

    Delete highscoreForm.js, form now tracks clicks through props and initial through form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import Home from './components/Home.js'
 import Rules from './components/Rules.js'
 import Leaderboard from './components/Leaderboard.js'
 import NavBar from './components/NavBar.js'
-import HighscoreForm from './components/HighscoreForm.js'
 import {
   BrowserRouter as Router,
   Route
--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -3,14 +3,17 @@ import React from 'react';
 export default class Info extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            value: this.props.value,
+            initials: ''
+        };
         
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
       }
     
       handleChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({initials: this.state.initials});
         //this will save user initals 
       }
     
@@ -27,7 +30,7 @@ export default class Info extends React.Component {
             </label>
             <label>
               Initials:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
+              <input type="text" value={this.state.initials} onChange={this.handleChange} />
             </label>
             <input type="submit" value="Submit" />
           </form>
