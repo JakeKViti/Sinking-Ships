b9b13e25950206b4192ac6682afd03315bdbc69a
JakeKViti <thevitij64@gmail.com>
2021-04-27 22:11:15 -0400

    added User Controller to API::V1, initize Cors, and created Users Table

--- frontend/src/components/Squid.js
+++ frontend/src/components/Squid.js
@@ -1,6 +1,36 @@
 import React from 'react';
 
 export default class Squid extends React.Component {
+    placeShips(data, height, width) {
+        data[0][0].ship = true;
+        data[0][1].ship = true;
+        data[1][0].ship = true;
+        data[1][1].ship = true;
+        data[1][2].ship = true;
+        data[2][0].ship = true;
+        data[2][1].ship = true;
+        data[2][2].ship = true;
+        data[2][3].ship = true;
+        //this.getRandomNumber(data, 2)
+        //this.getRandomNumber(data, 3)
+        //this.getRandomNumber(data, 4)
+        return (data);
+      }
+  
+      getRandomNumber(data, numbers) {
+        let x = Math.floor((Math.random() * 1000) + 1) % 17;
+        let y = Math.floor((Math.random() * 1000) + 1) % 17;
+        let ori = Math.floor((Math.random() * 1000) + 1) % 2;
+        for (let i = 0; i < numbers; i++) {
+          if (ori === 1) {
+            data[x+i][y].ship = true
+          } else {
+            data[x][y+i].ship = true
+          }
+        }
+        return 
+      }
+
     render() {
         return (
             <div>
